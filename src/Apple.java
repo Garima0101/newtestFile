da5df0d48e79245cab9a34343e36d68bcc1d5e8b
Garima0101 <106391540+Garima0101@users.noreply.github.com>
2022-12-03 17:10:15 -0800

    update apple

--- src/Apple.java
+++ src/Apple.java
@@ -3,7 +3,7 @@ public class Apple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Have a nice day");
+		System.out.println("Have a nice weekened");
 	}
 
 }
